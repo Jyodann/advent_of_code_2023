d2e08da2a1e5c18729d66a8af892d61f7b32676a
Jordyn <jordynwinnnie@gmail.com>
2023-12-17 21:12:38 +0800

    Day 9 FULL

---
+++ day_9/part-1/Program.cs
@@ -0,0 +1,52 @@
+﻿
+var lines = File.ReadAllLines("input");
+var number = 0;
+foreach (var line in lines)
+{
+    var numbers = line.Split(' ').Select(int.Parse);
+
+    Console.WriteLine(string.Join(',', numbers));
+
+    number +=    ReturnNextNum(numbers);
+
+}
+
+Console.WriteLine(number);
+
+int ReturnNextNum(IEnumerable<int> numbers) {
+    
+    var remainders = numbers.ToList();
+    var list_of_remainder = new List<List<int>>
+    {
+        remainders
+    };
+    do {
+        remainders = ReturnRemainders(remainders);
+
+        list_of_remainder.Add(remainders);
+    }
+    while (!remainders.All(x => x == 0)); 
+    
+    //list_of_remainder.Add(remainders);
+    var number = 0;
+    for (int i = list_of_remainder.Count - 1; i >= 0 ; i--)
+    {
+        var li = list_of_remainder[i];
+
+        number += li[^1];
+    }
+    return number;
+}
+
+List<int> ReturnRemainders(List<int> numbers) {
+    var remainders = new List<int>();
+    for (int i = 1; i < numbers.Count; i++)
+    {
+        var first_number = numbers[i - 1];
+        var second_number = numbers[i];
+
+        remainders.Add(second_number - first_number);
+    }
+
+    return remainders;
+}
---
+++ day_9/part-2/Program.cs
@@ -0,0 +1,56 @@
+﻿
+var lines = File.ReadAllLines("input");
+var number = 0;
+foreach (var line in lines)
+{
+    var numbers = line.Split(' ').Select(int.Parse);
+
+    Console.WriteLine(string.Join(',', numbers));
+
+    number +=    ReturnNextNum(numbers);
+}
+
+Console.WriteLine(number);
+
+int ReturnNextNum(IEnumerable<int> numbers) {
+    
+    var remainders = numbers.ToList();
+    var list_of_remainder = new List<List<int>>
+    {
+        remainders
+    };
+    do {
+        remainders = ReturnRemainders(remainders);
+
+        list_of_remainder.Add(remainders);
+    }
+    while (!remainders.All(x => x == 0)); 
+    
+    //list_of_remainder.Add(remainders);
+    var number = 0;
+    for (int i = list_of_remainder.Count - 1; i >= 1 ; i--)
+    {
+        var li = list_of_remainder[i];
+        var li_2 = list_of_remainder[i - 1];
+        var first_number = li[0];
+        var second_number = li_2[0];
+        
+        var new_num = second_number - number;
+        
+        number = new_num;
+    }
+    return number;
+}
+
+List<int> ReturnRemainders(List<int> numbers) {
+    var remainders = new List<int>();
+    for (int i = 1; i < numbers.Count; i++)
+    {
+        var first_number = numbers[i - 1];
+        var second_number = numbers[i];
+
+        remainders.Add(second_number - first_number);
+    }
+
+    return remainders;
+}
